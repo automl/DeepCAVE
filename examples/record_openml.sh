ca81a53460d98fe5fb1c7a1ae1f178fd87b2a368
renesass <mail@renesass.de>
2021-11-11 11:43:03 +0100

    Added background jobs

--- examples/record_openml.sh
+++ examples/record_openml.sh
@@ -3,9 +3,9 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-python record_openml.py --seed=0
-python record_openml.py --seed=1
-python record_openml.py --seed=2
-python record_openml.py --seed=3
-python record_openml.py --seed=4
-python record_openml.py --seed=5
+python record_openml.py --seed=0 &
+python record_openml.py --seed=1 &
+python record_openml.py --seed=2 &
+python record_openml.py --seed=3 &
+python record_openml.py --seed=4 &
+python record_openml.py --seed=5 &
