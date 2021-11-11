c133b8a61bb50e2faa733bb656461cc0602d89bd
renesass <mail@renesass.de>
2021-11-11 17:04:02 +0100

    Improved examples

--- examples/record_openml_rf.py
+++ examples/record_openml_rf.py
@@ -1,33 +1,39 @@
 import sys  # noqa
 sys.path.insert(0, '../')  # noqa
 
+from functools import partial
 from absl import app as flags_app
 from absl import flags
 from smac.facade.smac_hpo_facade import SMAC4HPO
 from smac.scenario.scenario import Scenario
-from hpobench.benchmarks.ml.nn_benchmark import NNBenchmark
+from hpobench.benchmarks.ml.rf_benchmark import RandomForestBenchmark
 from smac.initial_design.random_configuration_design import RandomConfigurations
 from src import Recorder
 
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('seed', 0, 'Seed', lower_bound=0)
-
-task_id = 167149
-n_configs = 500
-benchmark = NNBenchmark(task_id=task_id)
+flags.DEFINE_integer('n_configs', 500, '#Configurations', lower_bound=10)
+flags.DEFINE_integer('task_id', 167149, 'Task Id', lower_bound=0)
 
 
-def eval_func(cfg, seed=0):
+def eval_func(cfg, seed=0, benchmark=None):
     result_dict = benchmark.objective_function(
-        configuration=cfg, rng=seed, fidelity={"iter": 100})
+        configuration=cfg, rng=seed, fidelity={"n_estimators": 64})
     cost = result_dict['cost']
 
     return cost
 
 
 def main(argv):
+
     seed = FLAGS.seed
+    task_id = FLAGS.task_id
+    n_configs = FLAGS.n_configs
+
+    benchmark = RandomForestBenchmark(task_id=task_id)
     cs = benchmark.get_configuration_space(seed=seed)
+
+    tae = partial(eval_func, benchmark=benchmark)
 
     #-------- START SMAC --------#
     scenario = Scenario({
@@ -40,7 +46,7 @@ def main(argv):
 
     smac = SMAC4HPO(
         scenario=scenario,
-        tae_runner=eval_func,
+        tae_runner=tae,
         initial_design=RandomConfigurations,
         rng=seed)
     smac.optimize()
@@ -48,7 +54,7 @@ def main(argv):
     rh = smac.get_runhistory()
     very_start_time = None
 
-    with Recorder(cs, prefix=f"openml_{task_id}_smac_{seed}", overwrite=True) as r:
+    with Recorder(cs, prefix=f"rf_openml_{task_id}_smac_{seed}", overwrite=True) as r:
         for (config_id, instance_id, seed, budget), (cost, time, status, starttime, endtime, additional_info) in rh.data.items():
             config = rh.ids_config[config_id]
             if very_start_time is None:
@@ -61,10 +67,10 @@ def main(argv):
             r.end(costs=cost, end_time=endtime)
 
     #-------- START RANDOM SEARCH --------#
-    with Recorder(cs, prefix=f"openml_{task_id}_rs_{seed}", overwrite=True) as r:
+    with Recorder(cs, prefix=f"rf_openml_{task_id}_rs_{seed}", overwrite=True) as r:
         for config in cs.sample_configuration(n_configs):
             r.start(config)
-            cost = eval_func(config, seed)
+            cost = tae(config, seed)
             r.end(costs=cost)
 
 
