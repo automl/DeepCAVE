4d972c39361410ab0922afce9ef47cb60cafa68b
renesass <mail@renesass.de>
2021-11-19 11:52:35 +0100

    Updated fanova plugin

--- deepcave/plugins/fanova.py
+++ deepcave/plugins/fanova.py
@@ -1,24 +1,27 @@
 from typing import Dict, Type, Any
 
-from dash import dcc
+from dash import dcc, html
+from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 
 import pandas as pd
 import numpy as np
 
-from deepcave import app
 from deepcave.plugins.static_plugin import StaticPlugin
-from deepcave.plugins.dynamic_plugin import DynamicPlugin
 from deepcave.utils.logs import get_logger
+from deepcave.utils.data_structures import update_dict
+from deepcave.utils.styled_plotty import get_color
+from deepcave.utils.layout import get_slider_marks, get_select_options, get_checklist_options
+from deepcave.utils.compression import serialize, deserialize
 
 from deepcave.evaluators.fanova import fANOVA as _fANOVA
 
 
 logger = get_logger(__name__)
 
-"""
-class fANOVA(DynamicPlugin):
+
+class fANOVA(StaticPlugin):
     @staticmethod
     def id():
         return "fanova"
@@ -36,76 +39,82 @@ class fANOVA(DynamicPlugin):
         return "Hyperparameter Analysis"
 
     @staticmethod
-    def debug():
-        return False
+    def activate_run_selection():
+        return True
 
-    def get_input_layout(self):
+    @staticmethod
+    def get_input_layout(register):
         return [
             dbc.Label("Number of trees"),
-            dbc.Input(id=self.register_input(
-                "num_trees", "value"), type="number")
+            dbc.Input(id=register("num_trees", "value"))
         ]
 
-    def get_filter_layout(self):
+    @staticmethod
+    def get_filter_layout(register):
         return [
-            dbc.FormGroup([
+            html.Div([
                 dbc.Label("Hyperparameters"),
-                dbc.Checklist(id=self.register_input(
-                    "hyperparameters", ["options", "value"], filter=True)),
+                dbc.Checklist(
+                    id=register("hyperparameters", ["options", "value"])),
+            ], className="mb-3"),
 
+            html.Div([
+                dbc.Label("Budgets"),
+                dbc.Checklist(
+                    id=register("budgets", ["options", "value"])),
             ]),
-            dbc.FormGroup([
-                dbc.Label("Fidelities"),
-                dbc.Checklist(id=self.register_input(
-                    "fidelities", ["options", "value"], filter=True)),
-            ]),
-            dbc.FormGroup([
-                dbc.Label("Sort by"),
-                dbc.RadioItems(id=self.register_input(
-                    "sort", ["options", "value"], filter=True))
-            ])
         ]
 
-    def load_inputs(self, run):
-        hp_names = run.configspace.get_hyperparameter_names()
-        fidelities = run.get_budgets()
-
+    @staticmethod
+    def load_inputs(runs):
         return {
             "num_trees": {
                 "value": 16
             },
             "hyperparameters": {
-                "options": [{"label": name, "value": idx} for idx, name in enumerate(hp_names)],
-                "value": [i for i in range(len(hp_names))]
+                "options": get_checklist_options(),
+                "value": []
             },
-            "fidelities": {
-                "options": [{"label": name, "value": idx} for idx, name in enumerate(fidelities)],
-                "value": [i for i in range(len(fidelities))]
+            "budgets": {
+                "options": get_checklist_options(),
+                "value": []
             },
-            "sort": {
-                "options": [{"label": name, "value": idx} for idx, name in enumerate(fidelities)],
-                "value": len(fidelities) - 1
-            }
         }
 
-    def load_dependency_inputs(self, run, inputs):
+    @staticmethod
+    def load_dependency_inputs(runs, previous_inputs, inputs):
+        run = runs[inputs["run_name"]["value"]]
+        budgets = run.get_budgets()
+        hp_names = run.configspace.get_hyperparameter_names()
+
+        new_inputs = {
+            "hyperparameters": {
+                "options": get_checklist_options(hp_names),
+            },
+            "budgets": {
+                "options": get_checklist_options(budgets),
+            },
+        }
+        update_dict(inputs, new_inputs)
+
+        # Restrict to three hyperparameters
+        num_trees = inputs["num_trees"]["value"]
         try:
-            int(inputs["num_trees"]["value"])
+            int(num_trees)
         except:
-            self.update_alert("Only numbers are allowed.", color="danger")
-            inputs["num_trees"]["value"] = 16
+            inputs["num_trees"]["value"] = previous_inputs["num_trees"]["value"]
 
         return inputs
 
     @staticmethod
     def process(run, inputs):
         hp_names = run.configspace.get_hyperparameter_names()
-        fidelities = run.get_budgets()
+        budgets = run.get_budgets()
 
         # Collect data
         data = {}
-        for fidelity in fidelities:
-            X, Y = run.get_encoded_configs(budget=fidelity, for_tree=True)
+        for budget in budgets:
+            X, Y = run.get_encoded_configs(budget=budget, for_tree=True)
 
             evaluator = _fANOVA(
                 X, Y,
@@ -113,53 +122,73 @@ class fANOVA(DynamicPlugin):
                 num_trees=int(inputs["num_trees"]["value"])
             )
             importance_dict = evaluator.quantify_importance(
-                hp_names, depth=1, sorted=False)
+                hp_names,
+                depth=1,
+                sorted=False)
 
             importance_dict = {k[0]: v for k, v in importance_dict.items()}
 
-            data[fidelity] = importance_dict
+            data[budget] = importance_dict
 
         return data
 
-    def get_output_layout(self):
+    @staticmethod
+    def get_output_layout(register):
         return [
-            dcc.Graph(self.register_output("graph", "figure"))
+            dcc.Graph(register("graph", "figure"))
         ]
 
-    def load_outputs(self, filters, raw_outputs):
+    @staticmethod
+    def load_outputs(inputs, outputs, _):
+        run_name = inputs["run_name"]["value"]
+        outputs = outputs[run_name]
+        # First selected, should always be shown first
+        selected_hyperparameters = inputs["hyperparameters"]["value"]
+        selected_budgets = inputs["budgets"]["value"]
+
+        if len(selected_hyperparameters) == 0 or len(selected_budgets) == 0:
+            return PreventUpdate
+
+        # TODO: After json serialize/deserialize, budget is not an integer anymore
+        convert_type = type(selected_budgets[0])
+
         # Collect data
         data = {}
-        for fidelity_id, (fidelity, importance_dict) in enumerate(raw_outputs.items()):
-            if fidelity_id not in filters["fidelities"]["value"]:
+        for budget, importance_dict in outputs.items():
+            budget = convert_type(budget)
+
+            if budget not in inputs["budgets"]["value"]:
                 continue
 
             x = []
             y = []
             error_y = []
-            for hp_id, (hp_name, results) in enumerate(importance_dict.items()):
-                if hp_id not in filters["hyperparameters"]["value"]:
+            for hp_name, results in importance_dict.items():
+                if hp_name not in inputs["hyperparameters"]["value"]:
                     continue
 
                 x += [hp_name]
                 y += [results[1]]
                 error_y += [results[3]]
 
-            data[fidelity] = (
+            data[budget] = (
                 np.array(x),
                 np.array(y),
                 np.array(error_y)
             )
 
-            if filters["sort"]["value"] == fidelity_id:
-                selected_fidelity = fidelity
+            # if filters["sort"]["value"] == fidelity_id:
+            #    selected_fidelity = fidelity
 
         # Sort by last fidelity now
-        idx = np.argsort(data[selected_fidelity][1], axis=None)[::-1]
+        last_selected_budget = selected_budgets[-1]
+        idx = np.argsort(
+            data[last_selected_budget][1], axis=None)[::-1]
 
         bar_data = []
-        for fidelity, values in data.items():
+        for budget, values in data.items():
             bar_data += [go.Bar(
-                name=fidelity,
+                name=budget,
                 x=values[0][idx],
                 y=values[1][idx],
                 error_y_array=values[2][idx])
@@ -169,4 +198,3 @@ class fANOVA(DynamicPlugin):
         fig.update_layout(barmode='group')
 
         return [fig]
-"""
