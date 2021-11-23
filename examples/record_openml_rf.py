ed7193976cb45ba333b2ac79042d6b6ba5d74d16
renesass <mail@renesass.de>
2021-11-23 14:29:40 +0100

    Added objective

--- examples/record_openml_rf.py
+++ examples/record_openml_rf.py
@@ -5,11 +5,11 @@ from smac.facade.smac_hpo_facade import SMAC4HPO
 from smac.scenario.scenario import Scenario
 from hpobench.benchmarks.ml.rf_benchmark import RandomForestBenchmark
 from smac.initial_design.random_configuration_design import RandomConfigurations
-from deepcave import Recorder
+from deepcave import Recorder, Objective
 
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('seed', 0, 'Seed', lower_bound=0)
-flags.DEFINE_integer('n_configs', 500, '#Configurations', lower_bound=10)
+flags.DEFINE_integer('n_configs', 100, '#Configurations', lower_bound=10)
 flags.DEFINE_integer('task_id', 167149, 'Task Id', lower_bound=0)
 
 
@@ -26,6 +26,7 @@ def main(argv):
     seed = FLAGS.seed
     task_id = FLAGS.task_id
     n_configs = FLAGS.n_configs
+    objective = Objective("cost", lower=0)
 
     benchmark = RandomForestBenchmark(task_id=task_id)
     cs = benchmark.get_configuration_space(seed=seed)
@@ -41,6 +42,7 @@ def main(argv):
         "deterministic": True,
     })
 
+    """
     smac = SMAC4HPO(
         scenario=scenario,
         tae_runner=tae,
@@ -51,7 +53,7 @@ def main(argv):
     rh = smac.get_runhistory()
     very_start_time = None
 
-    with Recorder(cs, prefix=f"rf_openml_{task_id}_smac_{seed}", overwrite=True) as r:
+    with Recorder(cs, objectives=objective, save_path="logs/JJ", prefix=f"rf_openml_{task_id}_smac_{seed}", overwrite=True) as r:
         for (config_id, instance_id, seed, budget), (cost, time, status, starttime, endtime, additional_info) in rh.data.items():
             config = rh.ids_config[config_id]
             if very_start_time is None:
@@ -62,9 +64,10 @@ def main(argv):
 
             r.start(config, start_time=starttime)
             r.end(costs=cost, end_time=endtime)
+    """
 
     #-------- START RANDOM SEARCH --------#
-    with Recorder(cs, prefix=f"rf_openml_{task_id}_rs_{seed}", overwrite=True) as r:
+    with Recorder(cs, objectives=objective, save_path="logs/JJ", prefix=f"rf_openml_{task_id}_rs_{seed}", overwrite=True) as r:
         for config in cs.sample_configuration(n_configs):
             r.start(config)
             cost = tae(config, seed)
