0d37bb16d3d9fd985a6cabd75237e069d54ac468
dwoiwode <dominik.woiwode@stud.uni-hannover.de>
2022-01-24 11:35:51 +0100

    Fix AbstractRun problem

--- deepcave/runs/run.py
+++ deepcave/runs/run.py
@@ -308,6 +308,9 @@ class Run(AbstractRun, ABC):
     def get_objectives(self):
         return self.meta["objectives"]
 
+    def get_trials(self) -> Iterator['Trial']:
+        yield from self.history
+
     def get_objective_name(self, objective_names=None):
         """
         Get the cost name of given objective names.
