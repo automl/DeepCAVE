6d04d77a86ea019771345f40d7da8f1626e52517
René Sass <mail@renesass.de>
2022-03-31 11:44:59 +0200

    Added active navigation items

--- deepcave/layouts/sidebar.py
+++ deepcave/layouts/sidebar.py
@@ -23,11 +23,62 @@ class SidebarLayout(Layout):
         self.nav_points = nav_points
 
     def register_callbacks(self) -> None:
+        # Update navigation items
+        output = Output("navigation-items", "children")
+        input = Input("on-page-load", "pathname")
+
+        @app.callback(output, input)
+        def update_navigation_items(pathname):
+            layouts = []
+            for category, points in self.nav_points.items():
+                layouts += [
+                    html.H6(
+                        className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted",
+                        children=[html.Span(category)],
+                    )
+                ]
+
+                point_layouts = []
+                for (id, name, icon) in points:
+                    href = f"/plugins/{id}"
+                    point_layouts += [
+                        html.Li(
+                            className="nav-item",
+                            children=[
+                                html.A(
+                                    [html.I(className=icon), name],
+                                    className=f"nav-link {'active' if href == pathname else ''}",
+                                    href=href,
+                                )
+                            ],
+                        )
+                    ]
+
+                layouts += [html.Ul(className="nav flex-column", children=point_layouts)]
+
+            icon = {"data-feather": "file-text"}
+
+            return html.Div(
+                className="position-sticky pt-3",
+                children=[
+                    html.Ul(
+                        className="nav flex-column",
+                        children=[
+                            html.A(
+                                "General",
+                                className=f"nav-link {'active' if pathname == '/' else ''}",
+                                href="/",
+                            ),
+                        ],
+                    ),
+                    *layouts,
+                ],
+            )
+
         # Update queue information panel
-        @app.callback(
-            Output("queue-info", "children"),
-            Trigger("global-update", "n_intervals"),
-        )
+        output = Output("queue-info", "children")
+
+        @app.callback(output, Trigger("global-update", "n_intervals"))
         def update_queue_info():
             try:
                 jobs = {}
@@ -88,55 +139,11 @@ class SidebarLayout(Layout):
                 return
 
     def __call__(self) -> Union[List[Component], Component]:
-
-        layouts = []
-        for category, points in self.nav_points.items():
-            layouts += [
-                html.H6(
-                    className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted",
-                    children=[html.Span(category)],
-                )
-            ]
-
-            point_layouts = []
-            for (id, name, icon) in points:
-                point_layouts += [
-                    html.Li(
-                        className="nav-item",
-                        children=[
-                            html.A(
-                                [html.I(className=icon), name],
-                                className="nav-link",
-                                href=f"/plugins/{id}",
-                            )
-                        ],
-                    )
-                ]
-
-            layouts += [html.Ul(className="nav flex-column", children=point_layouts)]
-
-        icon = {"data-feather": "file-text"}
-
         return html.Nav(
             className="col-md-3 col-lg-2 d-md-block sidebar collapse",
             id="sidebarMenu",
             children=[
-                html.Div(
-                    className="position-sticky pt-3",
-                    children=[
-                        html.Ul(
-                            className="nav flex-column",
-                            children=[
-                                html.A(
-                                    "General",
-                                    className="nav-link active",
-                                    href="/",
-                                ),
-                            ],
-                        ),
-                        *layouts,
-                    ],
-                ),
+                html.Div(id="navigation-items"),
                 html.Div(id="queue-info"),
             ],
         )
