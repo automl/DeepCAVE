4ebc005b31a8922fd1103eebd73f8acaf3be0996
René Sass <mail@renesass.de>
2022-05-24 17:02:41 +0200

    Added table+dynamic text to budget correlation

--- deepcave/plugins/budget/budget_correlation.py
+++ deepcave/plugins/budget/budget_correlation.py
@@ -10,7 +10,7 @@ from scipy import stats
 from deepcave import notification
 from deepcave.plugins.dynamic import DynamicPlugin
 from deepcave.runs import AbstractRun, Status
-from deepcave.utils.layout import get_select_options
+from deepcave.utils.layout import create_table, get_select_options
 from deepcave.utils.logs import get_logger
 from deepcave.utils.styled_plotty import get_color
 
@@ -68,13 +68,20 @@ class BudgetCorrelation(DynamicPlugin):
     @staticmethod
     def process(run, inputs):
         objective_id = inputs["objective_id"]
+        budget_ids = run.get_budget_ids(include_combined=False)
+
+        # Add symmetric correlations; table ready
+        correlations_symmetric: Dict[str, Dict[str, float]] = defaultdict(dict)
+        # correlations_symmetric["/"] = budget_ids
 
         correlations: Dict[str, Dict[str, float]] = defaultdict(dict)
-        for budget1 in run.get_budgets(include_combined=False):
-            for budget2 in run.get_budgets(include_combined=False):
-                # Exclude if budget2 is higher than budget1
-                if budget2 > budget1:
-                    continue
+        for budget1_id in budget_ids:
+            budget1 = run.get_budget(budget1_id)
+            budget1_readable = run.get_budget(budget1_id, human=True)
+
+            for budget2_id in budget_ids:
+                budget2 = run.get_budget(budget2_id)
+                budget2_readable = run.get_budget(budget2_id, human=True)
 
                 costs1 = run.get_all_costs(budget1, statuses=[Status.SUCCESS])
                 costs2 = run.get_all_costs(budget2, statuses=[Status.SUCCESS])
@@ -88,24 +95,68 @@ class BudgetCorrelation(DynamicPlugin):
                     c1 += [costs1[config_id][objective_id]]
                     c2 += [costs2[config_id][objective_id]]
 
-                correlations[budget1][budget2] = round(stats.spearmanr(c1, c2).correlation, 2)
+                correlation = round(stats.spearmanr(c1, c2).correlation, 2)
+                correlations_symmetric["Budget"][budget2_readable] = budget2_readable
+                correlations_symmetric[budget1_readable][budget2_readable] = correlation
 
-        return {"correlations": correlations}
+                # Exclude if budget2 is higher than budget1
+                if budget2 > budget1:
+                    continue
+
+                correlations[budget1_readable][budget2_readable] = correlation
+
+        return {
+            "correlations": correlations,
+            "correlations_symmetric": correlations_symmetric,
+        }
 
     @staticmethod
     def get_output_layout(register):
-        return dcc.Graph(id=register("graph", "figure"))
+        return [
+            html.Div(id=register("text", "children"), className="mb-3"),
+            dbc.Tabs(
+                [
+                    dbc.Tab(dcc.Graph(id=register("graph", "figure")), label="Graph"),
+                    dbc.Tab(html.Div(id=register("table", "children")), label="Table"),
+                ]
+            ),
+        ]
 
     @staticmethod
-    def load_outputs(run, inputs, outputs):
+    def load_outputs(run: AbstractRun, inputs, outputs):
         traces = []
-
+        categories = defaultdict(list)
         correlations = outputs["correlations"]
+        correlations_symmetric = outputs["correlations_symmetric"]
         for idx, (budget1, budgets) in enumerate(correlations.items()):
             x, y = [], []
             for budget2, correlation in budgets.items():
                 x += [float(budget2)]
                 y += [correlation]
+
+                category = None
+                if correlation >= 0.7:
+                    category = "very strong"
+                elif correlation >= 0.4:
+                    category = "strong"
+                elif correlation >= 0.3:
+                    category = "moderate"
+                elif correlation >= 0.2:
+                    category = "weak"
+                else:
+                    category = "not given"
+
+                key = (budget1, budget2)
+                key2 = (budget2, budget1)
+
+                if budget1 == budget2:
+                    continue
+
+                if key not in categories[category] and key2 not in categories[category]:
+                    if budget1 < budget2:
+                        categories[category] += [key]
+                    else:
+                        categories[category] += [key2]
 
             traces.append(
                 go.Scatter(
@@ -119,12 +170,30 @@ class BudgetCorrelation(DynamicPlugin):
         layout = go.Layout(
             xaxis=dict(title="Budget"),
             yaxis=dict(title="Correlation"),
-            margin=dict(
-                t=30,
-                b=0,
-                l=0,
-                r=0,
-            ),
+            margin=dict(t=30, b=0, l=0, r=0),
         )
 
-        return [go.Figure(data=traces, layout=layout)]
+        text = "The budget correlation of"
+        n_categories = len(categories)
+        for i, (relation, pairs) in enumerate(categories.items()):
+            if n_categories > 1 and i == n_categories - 1:
+                text += ", and "
+            elif n_categories > 1 and i > 0:
+                text += ", "
+            else:
+                text += " "
+
+            formated_pairs = []
+            for pair in pairs:
+                formated_pairs += [f"({pair[0]}, {pair[1]})"]
+                
+            pairs_text = ", ".join(formated_pairs)
+            text += f"{pairs_text} is {relation}"
+            if i == n_categories - 1:
+                text += "."
+
+        return [
+            text,
+            go.Figure(data=traces, layout=layout),
+            create_table(correlations_symmetric, mb=False),
+        ]
