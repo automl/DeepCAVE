4e267ec91f3ab0228fc202b079f4b5e0d910b599
Kristina Thieme <krissithieme@gmx.de>
2024-04-17 14:49:58 +0200

    resolved some weird branch conflicts

--- deepcave/evaluators/ablation_importances.py
+++ deepcave/evaluators/ablation_importances.py
@@ -14,9 +14,7 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 
 import copy
 
-from sklearn.ensemble import (
-    RandomForestRegressor,  # TODO: Does that make sense as model?
-)
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
 from deepcave.constants import COMBINED_COST_NAME
@@ -34,8 +32,10 @@ class AblationImportances:
         self.cs = run.configspace
         self.hp_names = self.cs.get_hyperparameter_names()
         self.importances: Optional[Dict[Any, Any]] = None
+        # TODO: What makes sense to init here?
 
     def calculate(  # TODO: Change head
+        # TODO: Iterate a few times more (how often?) and average?
         # TODO: Iterate a few times more (how often?) and average?
         self,
         objectives: Optional[Union[Objective, List[Objective]]] = None,  # noqa
@@ -46,11 +46,14 @@ class AblationImportances:
     ) -> None:
         """Prepare the data for processing and train a Random Forest surrogate model."""
         # A Random Forest Regressor is used as surrogate
+        # A Random Forest Regressor is used as surrogate
         (
             self.default_config,
             incumbent_config,
-        ) = self._train_surrogate()  # TODO: Does it make sense to train on only one cs?
-
+        ) = (
+            self._train_surrogate()
+        )  # TODO: Does it make sense to train on only one cs? Yes but average it at the end,
+        # take the abweichung & use for the graphic as well
         res_default = self._model.predict([self.default])
 
         print("Default performance:", 1 - res_default)
@@ -59,8 +62,11 @@ class AblationImportances:
         importances = {}
 
         # Copy the hps names as to not remove objects from the original list
+
+        # Copy the hps names as to not remove objects from the original list
         hp_it = self.hp_names.copy()
         for i in range(len(hp_it)):
+            # Get the results of the current ablation iteration
             # Get the results of the current ablation iteration
             continue_ablation, max_hp, max_hp_performance, max_error = self._ablation(
                 incumbent_config, res_default, hp_it
@@ -71,6 +77,7 @@ class AblationImportances:
                 break
 
             print("Hyperparameter with max MSE", max_hp, "New performance:", max_hp_performance)
+            # Remove the current max hp for keeping the order right
             # Remove the current max hp for keeping the order right
             hp_it.remove(max_hp)
             importances[max_hp] = (max_hp_performance[0], max_error)
@@ -84,11 +91,15 @@ class AblationImportances:
         importances = {
             key: value for key, value in sorted(self.importances.items())
         }  # Why did i sort this?
+        importances = {
+            key: value for key, value in sorted(self.importances.items())
+        }  # Why did i sort this?
         return importances
 
     def _ablation(
         self, incumbent_config: Any, res_default: Any, hp_it: List[str]
     ) -> Tuple[Any, Any, Any, Any]:
+        # TODO: Check objectives for lower or upper -> only use hps that make a positive impact
         max_hp = ""
         max_hp_error = 0
         for hp in hp_it:
@@ -100,13 +111,16 @@ class AblationImportances:
                 res = self._model.predict(
                     [self.run.encode_config(config_copy)]
                 )  # TODO: Change the variable names
+                res = self._model.predict(
+                    [self.run.encode_config(config_copy)]
+                )  # TODO: Change the variable names
                 mse = mean_squared_error(res_default, res)
                 if mse > max_hp_error:
                     max_hp = hp
                     max_hp_error = mse  # TODO: Is this really the right way?
             else:
-                continue  # TODO: Maybe raise an error here? Does not seem ideal
-
+                continue
+                # TODO: Maybe raise an error here? Does not seem ideal
         if max_hp != "":
             self.default_config[max_hp] = incumbent_config[max_hp]
             max_hp_performance = self._model.predict([self.run.encode_config(self.default_config)])
@@ -115,9 +129,13 @@ class AblationImportances:
             print(
                 "No hyperparameter to ablate: ", max_hp, max_hp_error
             )  # TODO: This needs to be more clear what the problem is
+            print(
+                "No hyperparameter to ablate: ", max_hp, max_hp_error
+            )  # TODO: This needs to be more clear what the problem is
             return False, None, None, None
 
     def _train_surrogate(self) -> Tuple[Any, Any]:
+        # Collect the runs attributes for training the surrogate
         # Collect the runs attributes for training the surrogate
         objectives = self.run.get_objectives()
         budget = self.run.get_highest_budget()
@@ -125,9 +143,8 @@ class AblationImportances:
         df = self.run.get_encoded_data(objectives, budget, specific=True)
         X = df[self.run.configspace.get_hyperparameter_names()].to_numpy()
         # Combined cost name includes the cost of all selected objectives
-        Y = df[
-            COMBINED_COST_NAME
-        ].to_numpy()  # TODO: Change to wanted measure? At least BOHB does not work this way
+        Y = df[COMBINED_COST_NAME].to_numpy()  # TODO: Change to wanted measure,
+        # i think choosing diff objectives as user input, calculates the same
 
         # Only get first entry, the normalized cost is not needed
         incumbent_config = self.run.get_incumbent()[0]
@@ -135,12 +152,11 @@ class AblationImportances:
 
         default_config = (
             self.cs.get_default_configuration()
-        )  # TODO: Find a better fit than a random sample?
+        )  # TODO: Find a better fit than a random sample
         self.default = self.run.encode_config(default_config)
 
-        self._model = RandomForestRegressor(
-            max_depth=100, random_state=0
-        )  # TODO: Change parameters?
+        # TODO: Change parameters?
+        self._model = RandomForestRegressor(max_depth=100, random_state=0)
         self._model.fit(X, Y)
 
         return default_config, incumbent_config
