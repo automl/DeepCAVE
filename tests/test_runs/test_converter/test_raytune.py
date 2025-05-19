b326b44a65e9ec7b457e2c3ac38df6c27fa2a62c
Kristina Thieme <krissithieme@gmx.de>
2025-05-19 11:23:39 +0200

    deleted local path

--- tests/test_runs/test_converter/test_raytune.py
+++ tests/test_runs/test_converter/test_raytune.py
@@ -6,7 +6,8 @@ from deepcave.runs.converters.raytune import RayTuneRun
 
 class TestRayTune(unittest.TestCase):
     def test_test(self):
-        RayTuneRun.from_path(Path("/Users/krissi/Documents/DeepCAVE/logs/RayTune/run_3"))
+        run = RayTuneRun.from_path(Path("logs/RayTune/run_3"))
+        run.is_valid_run()
 
 
 if __name__ == "__main__":
