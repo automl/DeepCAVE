4fc4bc7b1fa883f24696373ba93eb80660a30eaa
Kristina Thieme <krissithieme@gmx.de>
2025-10-13 10:17:44 +0200

    added amltk validity pytest

---
+++ tests/test_runs/test_converter/test_amltk.py
@@ -0,0 +1,17 @@
+import unittest
+
+import pandas as pd
+
+from deepcave.runs.converters.amltk import AMLTKRun
+
+
+class TestOptunaConverter(unittest.TestCase):
+    def setUp(self) -> None:
+        self.run_path = "logs/AMLTK/optuna_optimizer/run_1"
+        return pd.set_option("display.max_columns", None)
+
+    def testValidRun(self):
+        AMLTKRun.is_valid_run(self.run_path)
+
+    def testReadRun(self):
+        AMLTKRun.from_path(self.run_path)
